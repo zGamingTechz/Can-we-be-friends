a6ad456cff44f5220befea623301c351c96eb559
Bhavya Soni <rkumarbhavy@gmail.com>
2025-04-29 17:24:53 +0530

    Added questions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3 as sql
 from models import User, db
