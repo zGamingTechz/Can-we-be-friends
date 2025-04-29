12ded258c7d0131ac73c2f045476c6d29648f28c
Bhavya Soni <rkumarbhavy@gmail.com>
2025-04-29 16:30:41 +0530

    Renamed index to home

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ def login():
 
 
 @app.route('/home', methods=["GET", "POST"])
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
