68455ee0b7d90e039d464c9285ec4dc9a2bcc30f
Bhavya Soni <rkumarbhavy@gmail.com>
2025-04-29 17:45:17 +0530

    Added session vars for email and qid, Added logic to store annswers in db and fetch and return questions.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3 as sql
 from models import User, db
+from questions import questions
 import keys
 
 app = Flask(__name__)
@@ -16,6 +17,9 @@ def login():
     if request.method == "POST":
         username = request.form['username']
         email = request.form['email']
+
+        # Store the email in session
+        session['email'] = email
 
         user = User.query.filter_by(email=email).first()
         if user:
@@ -36,7 +40,39 @@ def login():
 
 @app.route('/home', methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    if 'qid' not in session:
+        session['qid'] = 0
+
+    qid = session['qid']
+    user = User.query.filter_by(email=session['email']).first()
+
+    if request.method == "POST":
+        answer = request.form['choice']
+
+        if answer.lower() == 'yes':
+            answer_value = 1
+        elif answer.lower() == 'no':
+            answer_value = 0
+
+        setattr(user, f'q{qid}', answer_value)
+        db.session.commit()
+
+        session['qid'] += 1
+        if session['qid'] >= len(questions):
+            session.pop('qid')
+            return redirect()
+
+        return redirect("/home")
+
+    return render_template("home.html", question=questions[qid])
+
+
+@app.route("/clear")
+def clear_session():
+    session.pop("email", None)
+    session.pop("qid", None)
+    return "Session cleared!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
